8c76da2a4b6370bed4dc10849f9a35c9fbd7b209
German <germants100@gmail.com>
2024-05-15 22:09:34 -0600

    Using open AI API

--- chat.js
+++ chat.js
@@ -1,8 +1,24 @@
+ const API_KEY = ''
  
- 
- 
- 
- 
+ async function getCompletition() {
+   const res = await fetch('https://api.openai.com/v1/chat/completions', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + API_KEY,
+            body : JSON.stringify({
+                model : 'gpt-3.5-turbo-16k',
+                prompt: 'Dame un nombre aleatorio de un leguaje de programacion',
+                max_tokens: 5
+            })
+        }
+    })
+
+    const data = await res.json();
+    console.log(data)
+ }
+
+//  getCompletition()
  
  
  
