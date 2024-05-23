a61cd9d9d98600b507aa7118610d13a63bac08f8
kyotoxkobe <freaklopip@outlook.com.ar>
2024-05-22 23:30:34 -0600

    Remove sensitive information

--- src/js/home.ts
+++ src/js/home.ts
@@ -12,5 +12,26 @@ document.addEventListener('DOMContentLoaded', async () => {
         const userHtml = document.getElementById('userName');
         if (userHtml) userHtml.innerHTML = user?.email?.split('@')[0] ?? ''
     } 
+
+    document.getElementById('userName')?.addEventListener('mouseover', function() {
+        document.getElementById('myDropdown')?.classList.add('show');
+    });
+    
+    document.getElementById('userName')?.addEventListener('mouseout', function() {
+        setTimeout(() => {
+            if (!document.querySelector('#myDropdown:hover')) {
+                document.getElementById('myDropdown')?.classList.remove('show');
+            }
+        }, 200);
+    });
+    
+    document.getElementById('myDropdown')?.addEventListener('mouseover', function() {
+        this.classList.add('show');
+    });
+    
+    document.getElementById('myDropdown')?.addEventListener('mouseout', function() {
+        this.classList.remove('show');
+    });
+    
 })
 
